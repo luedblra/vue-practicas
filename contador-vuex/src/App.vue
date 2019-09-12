5ff5f7b2c8af401f7f373a3824f7e1e07c43ed1e
luis <luedblra@gmail.com>
2019-09-12 09:26:35 -0400

    se inicia  con vuex

---
+++ contador-vuex/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <p>Hooola Luis</p>
+      <h5>{{message}}</h5>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+    data(){
+        return {
+            message: 'Hola desde export'
+        }
+    }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
